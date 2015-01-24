a737cf43f3762e543899254c9217fd9c82d23b32
Mark Goldenson <mjgold1@gmail.com>
2015-01-24 14:48:56 -0800

    Use relative image paths

--- gallery.rb
+++ gallery.rb
@@ -6,24 +6,22 @@ require 'FileUtils'
 
 ### METHODS
 
-def get_file_paths(arguments)
+def file_paths(arguments)
   file_paths = []
   arguments.each do |arg|
-    file_paths << File.absolute_path(arg)
+    file_paths << File.path(arg)
   end
 
   file_paths
 end
 
-def get_img_tag(filepath)
+def img_tag(filepath)
   "<img src=\"#{filepath}\">"
 end
 
 # Stylesheet hardcoded for now
-def get_html(img_tags)
-  # joined_img_tags = img_tags.map { |tag| "#{tag}\n" }.join
-
-  html = <<-HTML
+def full_html(img_tags)
+  html_top = <<-HTML_TOP
   <!DOCTYPE html>
   <html>
   <head>
@@ -33,34 +31,34 @@ def get_html(img_tags)
   <body>
     <h1 id='topheader'>My Gallery</h1>
     <div class="section group">
-  HTML
+  HTML_TOP
 
   img_html = add_css_to_img_tags(img_tags)
 
-  html2 = <<-HTML2
+  html_bottom = <<-HTML_BOTTOM
     </div>
   </body>
   </html>
-  HTML2
+  HTML_BOTTOM
 
-  html + img_html + html2
+  html_top + img_html + html_bottom
 end
 
 def add_css_to_img_tags(img_tags)
   img_html = ''
   img_tags.each_with_index do |tag, i|
     col_num = i % 3 + 1
-    img_html += make_div_tag(tag, col_num)
+    img_html += div_tag(tag, col_num)
   end
 
   img_html
 end
 
-def make_div_tag(tag, col_num)
+def div_tag(tag, col_num)
   "<div class='col span_#{col_num}_of_3'>#{tag}</div>"
 end
 
-def create_directory(dir = 'public', img_dir = 'images')
+def create_dir(dir = 'public', img_dir = 'images')
   new_dir = "#{dir}/#{img_dir}"
   FileUtils.mkdir_p new_dir
 
@@ -69,33 +67,33 @@ end
 
 ### MAIN
 
-file_paths = get_file_paths(ARGV)
+file_paths = file_paths(ARGV)
+new_dir = create_dir # Get user-specified directories later
+FileUtils.copy(file_paths, new_dir)
 
 img_tags = []
 file_paths.each do |filepath|
-  img_tags << get_img_tag(filepath)
+  img_tags << img_tag(filepath)
 end
 
-html = get_html(img_tags)
+html = full_html(img_tags)
 File.write('gallery.html', html)
-new_dir = create_directory # Get user-specified directories later
-FileUtils.copy(file_paths, new_dir)
 
 ### TESTS
 
 if __FILE__ == $PROGRAM_NAME
 
   arguments = ['photos/bunny1.jpg']
-  file_paths = get_file_paths(arguments)
-  p get_img_tag(file_paths[0])
-  p get_img_tag(file_paths[0]) == \
-    "<img src=\"/Users/mark/Dropbox/Code/photo-gallery-cli/photos/bunny1.jpg\">"
+  file_paths = file_paths(arguments)
+  # p img_tag(file_paths[0])
+  p img_tag(file_paths[0]) == \
+    "<img src=\"photos/bunny1.jpg\">"
 
   arguments = ['photos/bunny1.jpg', 'photos/bunny2.jpg']
-  file_paths = get_file_paths(arguments)
-  p get_img_tag(file_paths[0]) == \
-    "<img src=\"/Users/mark/Dropbox/Code/photo-gallery-cli/photos/bunny1.jpg\">"
-  p get_img_tag(file_paths[1]) == \
-    "<img src=\"/Users/mark/Dropbox/Code/photo-gallery-cli/photos/bunny2.jpg\">"
+  file_paths = file_paths(arguments)
+  p img_tag(file_paths[0]) == \
+    "<img src=\"photos/bunny1.jpg\">"
+  p img_tag(file_paths[1]) == \
+    "<img src=\"photos/bunny2.jpg\">"
 
 end
