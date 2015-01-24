873ff95a373366933daf030f5800d5d1105ce4a1
Mark Goldenson <mjgold1@gmail.com>
2015-01-23 16:19:48 -0800

    Trying to implement 3-column CSS grid.

--- gallery.rb
+++ gallery.rb
@@ -1,62 +1,76 @@
+# Creates a photo gallery from photo files passed through the command line
 
-# user enters ruby gallery.rb <photo1 path> <photo2 path> <etc.>
-# create an HTML page 
-#   create HTML header
-#   create HTML table
-#   for each photo, insert <img tags>
-#   create HTML footer
+# Example: ruby gallery.rb photos/bunny1.jpg photos/bunny2.jpg
 
 def get_file_paths(arguments)
-	file_paths = Array.new
-	arguments.each do |arg|
-		file_paths << Dir.pwd + "/" + arg
-	end
+  file_paths = []
+  arguments.each do |arg|
+    file_paths << Dir.pwd + '/' + arg
+  end
 
-	file_paths
+  file_paths
 end
 
 def get_img_tag(filepath)
-	img_tag = "<img src=\"" + filepath + "\">"
+  "<img src=\"" + filepath + "\">"
 end
 
-def get_HTML(img_tags) 
+# Stylesheet hardcoded for now
+def get_html(img_tags)
+  # joined_img_tags = img_tags.map { |tag| "#{tag}\n" }.join
 
-  joined_img_tags = img_tags.map { |tag| "#{tag}\n"}.join
-
-	html = <<-HTML
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<title>My Gallery</title>
-	</head>
-	<body>
-		<h1>My Gallery</h1>
-		#{joined_img_tags}
-	</body>
-	</html>
+  html = <<-HTML
+  <!DOCTYPE html>
+  <html>
+  <head>
+		<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+    <title>My Gallery</title>
+  </head>
+  <body>
+    <h1>My Gallery</h1>
+		<div class="section group">
 	HTML
 
-	html
+	img_html = ""
+	img_tags.each_with_index do |tag, col|
+		img_html += '<div class="col span_' + \
+		((col % 3) + 1).to_s + \
+		'_of_3">' + \
+		tag + \
+		'</div>'
+	end
+
+	html2 = <<-HTML2
+		</div>
+  </body>
+  </html>
+  HTML2
+
+	html + img_html + html2
 end
 
 file_paths = get_file_paths(ARGV)
 
-img_tags = Array.new
+img_tags = []
 file_paths.each do |filepath|
-	img_tags << get_img_tag(filepath)
+  img_tags << get_img_tag(filepath)
 end
 
-html = get_HTML(img_tags)
-File.write("gallery.html", html)
+html = get_html(img_tags)
+File.write('gallery.html', html)
 
-if __FILE__ == $0
+if __FILE__ == 'gallery.rb'
 
-	arguments = ["photos/bunny1.jpg"]
-	file_paths = get_file_paths(arguments)	
-	
-	arguments = ["photos/bunny1.jpg", "photos/bunny2.jpg"]
-	file_paths = get_file_paths(arguments)	
-	p get_img_tag(file_paths[0]) == "<img src=\"/Users/mark/Dropbox/Code/photo-gallery-cli/photos/bunny1.jpg\">"
-	p get_img_tag(file_paths[1]) == "<img src=\"/Users/mark/Dropbox/Code/photo-gallery-cli/photos/bunny2.jpg\">"
-	
+  arguments = ['photos/bunny1.jpg']
+  file_paths = get_file_paths(arguments)
+	p get_img_tag(file_paths[0]) == \
+	  "<img src=\"/Users/mark/Dropbox/Code/photo-gallery-cli/photos/bunny1.jpg\">"
+
+  arguments = ['photos/bunny1.jpg', 'photos/bunny2.jpg']
+  file_paths = get_file_paths(arguments)
+  p get_img_tag(file_paths[0]) == \
+	  "<img src=\"/Users/mark/Dropbox/Code/photo-gallery-cli/photos/bunny1.jpg\">"
+  p get_img_tag(file_paths[1]) == \
+	  "<img src=\"/Users/mark/Dropbox/Code/photo-gallery-cli/photos/bunny2.jpg\">"
+
 end
